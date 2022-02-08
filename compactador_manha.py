f643dd77cda2dae71187a22e2ccce8411447a32d
Marcelo Caon <marcelocaon@gmail.com>
2022-02-08 16:42:08 -0300

    Add files via upload

---
+++ compactador_manha.py
@@ -0,0 +1,15 @@
+# importing required modules
+from zipfile import ZipFile
+import os
+
+
+def compactar_arquivo(nome_arquivo):
+    # writing files to a zipfile
+    with ZipFile('sddb_m.zip', 'w') as zip:
+        # writing each file one by one
+        zip.write(nome_arquivo)
+
+    print('File zipped successfully!')
+
+
+compactar_arquivo('SDDB_M.FBK')
